8adc32322601d74fd9aec2b2e146d2a46da223fd
茈 <110022939+dcbbsp@users.noreply.github.com>
2023-08-20 20:27:16 +0800

    Add files via upload
    2023/8/20 基础语法~数据类型

---
+++ C+/1.cpp
@@ -0,0 +1,14 @@
+// <基础语法>>
+#include <iostream> // 头文件
+using namespace std;// 命名空间
+
+int main() { // 主要
+    cout << "喜欢伊蕾娜Q-Q";
+    return 0; // 返回 0 <-注释
+}
+/*
+多 年 祝
+行 年 你
+注 有 幸
+释 鱼 福
+*/
